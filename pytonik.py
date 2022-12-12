7dc35090aa096684a1f3702308d9c171554bf9ca
Karol Leśniewski <lesiulesniewski@gmail.com>
2022-12-12 17:40:12 +0100

    I've done second quest :)

--- pytonik.py
+++ pytonik.py
@@ -8,3 +8,18 @@ print(result)
 print("asdasd, asdasdas, adsdadaddd")
 
 print()
+
+#second quest is done, i hope :)
+def parametr_required(a, b, c, x = 10, z = 15):
+
+    return a + b + c + x + z
+result = parametr_required(1, 2, 3)
+print(result)
+
+
+
+def f(x):
+    a = int(10)
+    return x / 2
+
+    
