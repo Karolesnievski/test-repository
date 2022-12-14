432bdbf6d078eb6cb09b28fc282e4d5f83e7853a
Karol Leśniewski <lesiulesniewski@gmail.com>
2022-12-14 13:12:32 +0100

    I'v done

--- pytonik.py
+++ pytonik.py
@@ -1,25 +1,7 @@
-def f(x):
-    return x * 2
-
-result = f(10)
+def first_quest(x):
+    return x ** 2
+result = first_quest(10)
 print(result)
 
-
-print("asdasd, asdasdas, adsdadaddd")
-
-print()
-
-#third quest is done, i hope :)
-def parametr_required(a, b, c, x = 10, z = 15):
-
-    return a + b + c + x + z
-result = parametr_required(1, 2, 3)
-print(result)
-
-
-
-def f(x):
-    a = int(10)
-    return x / 2
-
-    
+def second_quest(asdas, super, asdsada):
+    print(second_quest)
