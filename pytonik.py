c7466ea5f2291f3db89e174db115aeaae4e43ef6
Karol Leśniewski <lesiulesniewski@gmail.com>
2022-12-15 11:07:48 +0100

    i starting to begin

--- pytonik.py
+++ pytonik.py
@@ -1,13 +1,4 @@
-def first_quest(x):
+def functiones(x):
     return x ** 2
-result = first_quest(10)
-print(result)
-
-def second_quest(string):
-    print(string)
-second_quest("warior")
-
-def third_quest(x):
-    return x // 2
-result = third_quest(2)
-print(result)
+    functiones(result)
+    result(10)
