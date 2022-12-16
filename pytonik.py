7011ca478daecf8ea0a25b59c706e0a423470df3
Karol Leśniewski <lesiulesniewski@gmail.com>
2022-12-16 23:15:39 +0100

    ALL DONE

--- pytonik.py
+++ pytonik.py
@@ -1,5 +1,6 @@
 # 1
 def firstt(x):
+
     return x ** 2
 print(firstt(8))
 
@@ -7,6 +8,13 @@ print(firstt(8))
 def second():
     print("asdsa", "adsasda", "asdasgffg")
 second()
+
+#3
+def hihihi(z, x, c, a = 100, s = 250):
+    return z + x + c + a + s
+result = hihihi(1, 1, 1)
+print(result)
+
 
 # 4 
 def fourth(x):
@@ -16,5 +24,15 @@ def multi(x):
 y = fourth(1)
 z = multi(y)
 print(z)
+
+# 5
+def conv(string):
+    try: 
+       return float(string)
+    except ValueError:
+        print("nain")
+c = conv("55.0")
+print(c)
+
 
 
